a5ca2ec6d8eda1886ef9574232b76dedce885637
Vitor Nunes <vitor1nuness@gmail.com>
2024-02-03 10:38:45 -0300

    feat: implemments relations on todos and categories

--- todo/app/Models/Todo.php
+++ todo/app/Models/Todo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Todo extends Model
 {
@@ -12,5 +13,11 @@ class Todo extends Model
     protected $fillable = [
         'description',
         'is_complete',
+        'category',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
