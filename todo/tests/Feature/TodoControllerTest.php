bb824b6e4d90ab41960448ea1cb4f42f950821df
Vitor Nunes <vitor1nuness@gmail.com>
2024-02-07 22:06:23 -0300

    feat: implement test todo show method

--- todo/tests/Feature/TodoControllerTest.php
+++ todo/tests/Feature/TodoControllerTest.php
@@ -13,16 +13,41 @@ class TodoControllerTest extends TestCase
     /**
      * A test to get data from todos table.
      */
-    public function test_todo_index(): void
+    public function test_todo_index_empty(): void
     {
         $response = $this->get('/todos');
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertJsonStructure(["data"]);
+    }
+
+    public function test_todo_index_json_sctructure(): void
+    {
+        $data = [
+            "description" => 'Todo 1',
+            "is_complete" => 1
+        ];
+        $this->post('/todos', $data);
+
+        $response = $this->get('/todos');
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                "data" => [
+                    [
+                        "id",
+                        "description",
+                        "is_complete",
+                        "category",
+                        "created_at",
+                        "updated_at",
+                    ],
+                ],
+            ]);
     }
 
     /**
      * A test to store values into todos table.
      */
-    public function test_todo_store():void
+    public function test_todo_store(): void
     {
         $data = [
             "description" => 'Todo 1',
@@ -30,13 +55,45 @@ class TodoControllerTest extends TestCase
         ];
 
         $response = $this->post('/todos', $data);
-        $response->assertCreated();
+        $response->assertCreated()
+            ->assertJsonStructure([
+                "data" => [
+                    "id",
+                    "description",
+                    "is_complete",
+                    "category",
+                    "created_at",
+                    "updated_at",
+                ],
+            ]);
+    }
+
+    public function test_todo_show():void
+    {
+        $data = [
+            "description" => 'Todo 1',
+            "is_complete" => 1
+        ];
+
+        $this->post('/todos', $data);
+        $response = $this->get('/todos/1');
+        $response->assertOk()
+            ->assertJsonStructure([
+                "data" => [
+                    "id",
+                    "description",
+                    "is_complete",
+                    "category",
+                    "created_at",
+                    "updated_at",
+                ],
+            ]);
     }
 
     /**
      * A test to verify validations on create and update.
      */
-    public function test_store_update_validations():void
+    public function test_store_update_validations(): void
     {
         $data = [
             "description" => null,
@@ -50,7 +107,7 @@ class TodoControllerTest extends TestCase
     /**
      * A test to update a value into todos table.
      */
-    public function test_todo_update():void
+    public function test_todo_update(): void
     {
         $data = [
             "description" => 'Todo 1',
@@ -62,13 +119,23 @@ class TodoControllerTest extends TestCase
             "is_complete" => 1
         ];
         $response = $this->patch('/todos/1', $data);
-        $response->assertOk();
+        $response->assertOk()
+        ->assertJsonStructure([
+            "data" => [
+                "id",
+                "description",
+                "is_complete",
+                "category",
+                "created_at",
+                "updated_at",
+            ],
+        ]);
     }
 
     /**
      * A test to delete value by id from todos table.
      */
-    public function test_todo_delete():void
+    public function test_todo_delete(): void
     {
         $data = [
             "description" => 'Todo 1',
